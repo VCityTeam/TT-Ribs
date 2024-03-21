4ff30f45ed9ca83d824a5739db11455363dfe03c
EricBoix <eric.boix@insa-lyon.fr>
2024-03-21 17:17:37 +0100

    Euler characteristic now asserted.

--- README.md
+++ README.md
@@ -6,13 +6,23 @@ A Blender based point cloud generator of mockups of Lyon fish-bones
 * Blender version > 4.0.2
 
 ## Exporting to ply
+
+Using the blender binary
 ```bash
 cd Src
 blender --background ../Blender/Cave_V5_ready_toscript.blend --python export_to_ply.py
 ```
 
-Open the resulting file (`bozo.ply`) e.g. with `https://point.love/`
+The python module way (avoids Blender UI installation) 
+```bash
+cd Src
+python3.10 -m venv venv-3.10
+source venv-3.10/bin/activate
+pip3.10 install bpy
+python export_to_ply.py
+```
+
+Open the resulting file (`result_*.ply`) e.g. with `https://point.love/`
 
 ## TODO
 * Try the `apply_modifiers=True` exporting option.
-* Try to assert Euler Characteristics before exportation
--- Src/export_to_ply.py
+++ Src/export_to_ply.py
@@ -1,18 +1,38 @@
 import bpy
+from bmesh_utils import bmesh_get_boundaries, bmesh_assert_genus_number_boundaries
+from UI_utils import demote_UI_object_with_mesh_to_bmesh
+import logging
+import sys
+
+prescribed_subdivision_level = 4
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename="export_to_ply.log", encoding="utf-8", level=logging.DEBUG)
+
+bpy.ops.wm.open_mainfile(filepath="../Blender/Cave_V5_ready_toscript.blend")
 
 cave = bpy.data.objects["Cave"]
-cave.select_set(True)
 
-# bpy.context.space_data.context = "MODIFIER"
+subdiv = cave.modifiers["Subdivision"]
+subdiv.levels = prescribed_subdivision_level
+
 bpy.ops.object.modifier_apply(modifier="Subdivision")
 bpy.ops.object.modifier_apply(modifier="Displace.ground")
 bpy.ops.object.modifier_apply(modifier="Displace.walls")
 bpy.ops.object.modifier_apply(modifier="Displace_structure")
 
-# bpy.context.space_data.context = "RENDER"
+cave_bmesh = demote_UI_object_with_mesh_to_bmesh(cave)
+bmesh_assert_genus_number_boundaries(
+    cave_bmesh,
+    5,  # Genus
+    4,  # Number of boundaries
+    "The topology of the cave is wrong.",
+)
+print("Boundaries: ", len(bmesh_get_boundaries(cave_bmesh)))
+
 bpy.ops.object.bake(type="COMBINED")
 bpy.ops.wm.ply_export(
-    filepath="bozo.ply",
+    filepath="result_" + str(prescribed_subdivision_level) + ".ply",
     check_existing=True,
     forward_axis="Y",
     up_axis="Z",
